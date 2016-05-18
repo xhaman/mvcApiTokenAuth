c5bcb1a86354b82b0abab74c34dc4cd61bd00978
xhaman <xhaman1@gmail.com>
2016-05-18 16:03:05 -0500

    First Commit

---
+++ TokenAuth/AngularJSAuthentication/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(AngularJSAuthentication.API.Startup))]
+
+namespace AngularJSAuthentication.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
